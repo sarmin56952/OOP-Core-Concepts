2613b47af56f11a2690c6b8dd3c4d6f239dd50e3
sarmin56952 <sarmin56952@gmail.com>
2026-06-14 09:40:20 +0600

    Create destructor.cpp

---
+++ Constructor_Destructor/destructor.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Employee{
+    public:
+    Employee()
+    {
+        cout<<"Constructor Invoked"<<endl;
+    }
+    ~Employee()
+    {
+        cout<<"Destructor Invoked"<<endl;
+    }
+
+};
+int main()
+{
+    Employee e1;
+    Employee e2;
+
+}
